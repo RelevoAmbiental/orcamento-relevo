8ffe11d1f7212de3dcd3e12d1dc0ecfa33e26626
Relevo Consultoria Ambiental <samuel@relevo.eco.br>
2025-11-08 20:35:52 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // raiz do domínio principal — certo!
+  base: '/', // raiz do domínio
   build: {
     outDir: 'dist',
-    sourcemap: false,
-  },
-  server: {
-    mimeTypes: {
-      // garante que .jsx e .js sejam servidos corretamente no dev
-      'text/javascript': ['js', 'jsx'],
-    },
   },
 });
