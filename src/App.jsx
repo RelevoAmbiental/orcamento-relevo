b8b30ca3402712076e527d8a3b91c626034fbfd7
Relevo Consultoria Ambiental <samuel@relevo.eco.br>
2025-11-07 22:29:15 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-// src/App.jsx
 import React from 'react';
 import { OrcamentoProvider } from './context/OrcamentoContext';
+
+// Componentes principais
 import Header from './components/Header';
+import IdentificacaoOrcamento from './components/IdentificacaoOrcamento';
 import CustosGerais from './components/CustosGerais';
 import Coordenacao from './components/Coordenacao';
 import Profissionais from './components/Profissionais';
@@ -13,13 +15,22 @@ function App() {
   return (
     <OrcamentoProvider>
       <div className="min-h-screen bg-gray-100">
+        {/* Cabeçalho com logo e botões */}
         <Header />
-        <div className="container mx-auto px-4 py-6">
+
+        {/* Conteúdo principal */}
+        <div className="container mx-auto px-4 py-6 space-y-6">
+          {/* Identificação do orçamento */}
+          <IdentificacaoOrcamento />
+
+          {/* Seções principais */}
           <CustosGerais />
           <Coordenacao />
           <Profissionais />
           <ValoresUnicos />
           <Logistica />
+
+          {/* Resumo com total e desconto */}
           <ResumoTotal />
         </div>
       </div>
