a48e73f8e13541382cc4463ff94f9f6650e6fe36
Relevo Consultoria Ambiental <samuel@relevo.eco.br>
2025-11-07 22:06:18 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// src/App.jsx - VERSÃO CORRIGIDA
+// src/App.jsx
 import React from 'react';
 import { OrcamentoProvider } from './context/OrcamentoContext';
 import Header from './components/Header';
@@ -14,9 +14,7 @@ function App() {
     <OrcamentoProvider>
       <div className="min-h-screen bg-gray-100">
         <Header />
-        
         <div className="container mx-auto px-4 py-6">
-          {/* REMOVI O GerenciarOrcamentos DAQUI - AGORA ESTÁ NO HEADER */}
           <CustosGerais />
           <Coordenacao />
           <Profissionais />
