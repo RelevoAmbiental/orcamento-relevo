21b2c742a51943ee36268a6199d4365e68dee34f
Relevo Consultoria Ambiental <samuel@relevo.eco.br>
2025-11-11 11:57:02 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     allowedHosts: ['5173-iet5z0jheqmlp9n6seny2-62b4a251.manus.computer'],
   },
   plugins: [react()],
-  base: '/orcamento/', // ✅ SUBPASTA DO PORTAL
+  base: '/orcamento/', // ✅ MUDANÇA CRÍTICA
   build: {
     outDir: 'dist',
     sourcemap: false
